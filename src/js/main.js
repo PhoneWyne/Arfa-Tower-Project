3aa1f5cb7aad9cc357c6f337c1494575deea5f48
PhoneWyne <85339083+PhoneWyne@users.noreply.github.com>
2025-04-08 08:24:49 +0500

    Added card adding to cart functionality, storing their data-Ahmed

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,30 @@
 import { loadComponent } from "./loadComponent.js";
 import { getCartCount, addToCart } from "./cartStorage.js";
 import { renderCart } from "./cart.js";
+import { renderProductCard } from "./renderProductCard.js";
+
+const products = [
+    {
+      id: 1,
+      name: "Ajax Home Kit",
+      price: 79.99,
+      image: "./src/images/product/jersey1.png"
+    },
+    {
+      id: 2,
+      name: "Albania Away Kit",
+      price: 79.99,
+      image: "./src/images/product/jersey2.jpeg"
+    }
+];
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const container = document.querySelector(".product-grid");
+    for (const product of products) {
+      const card = await renderProductCard(product);
+      container.appendChild(card);
+    }
+});
 
 // Load navbar and footer on all pages
 document.addEventListener("DOMContentLoaded", function () {
